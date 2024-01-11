adc84903450cfaf66241cd53f68eff1cf1a38212
KSE <fplls12@gmail.com>
2024-01-12 00:18:01 +0900

    포탈 생성 로직 색 별로 나눔

---
+++ Source/PortalProject/Private/Object/Portal_CloseBullet.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Object/Portal_CloseBullet.h"
+
+// Sets default values
+APortal_CloseBullet::APortal_CloseBullet()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APortal_CloseBullet::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APortal_CloseBullet::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
