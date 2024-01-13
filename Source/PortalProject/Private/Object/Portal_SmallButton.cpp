51bd03bf21c17289d48c8617bfc497d58ac3e2dd
KSE <fplls12@gmail.com>
2024-01-13 20:26:19 +0900

    버튼 클래스 생성 E 입력키 버튼과 큐브 드는 기능 2개로 분리, 라인트레이스 를 이용하여 버튼과 큐브 구분기능 추가 큐브 들기 또한 라인트레이스로 처리

---
+++ Source/PortalProject/Private/Object/Portal_SmallButton.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Object/Portal_SmallButton.h"
+
+// Sets default values
+APortal_SmallButton::APortal_SmallButton()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APortal_SmallButton::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APortal_SmallButton::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
