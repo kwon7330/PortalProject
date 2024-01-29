ad7898f009951912790e20f8e43163a566e9bf36
singateco <singateco@gmail.com>
2024-01-29 17:41:22 +0900

    애니메이션 수정

--- PortalProject.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\Epic Games\UE_5.3\Engine\Binaries\Win64\UnrealEditor.exe" "C:\PortalProject\PortalProject\PortalProject.uproject" -game -log -ResX = 600 -ResY=400 -WinX=10 -WinY=100
-fdsa
